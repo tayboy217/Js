1e5986fd469a10148b23eeae289ce0432ec86599
tayboy217 <tayboy217@icloud.com>
2022-02-02 22:51:39 +0900

    [Finish]QuizGame

--- Js1/main.js
+++ Js1/main.js
@@ -26,11 +26,12 @@ const quiz = [
       'I eat a durian'
     ],
     correct: 'I like an apple'
-  }
+  },
 ]
 
 const quizLength = quiz.length;
 let quizIndex = 0;
+let score = 0;
 
 
 const $button = document.getElementsByTagName('button');
@@ -50,6 +51,7 @@ setupQuiz();
 const clickHandler = (e) => {
   if(quiz[quizIndex].correct === e.target.textContent){
     window.alert('正解！');
+    score++;
   } else {
     window.alert('不正解!');
   }
@@ -61,7 +63,7 @@ const clickHandler = (e) => {
     setupQuiz();
   }　else　{
     //問題数がもうなければこちらを実行
-    window.alert('終了！')
+    window.alert('終了！あなたの正解数は' + score + '/' + quizLength + 'です！');
   }
 };
 
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Quiz Game with JavaScript
+
+## クイズーム
+https://user-images.githubusercontent.com/90984182/152166231-6fa5ab2d-ffd0-4ecc-ba27-c7fa4a83fdc9.mov
